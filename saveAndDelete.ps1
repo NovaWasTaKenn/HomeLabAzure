8b46f7b4b1c2908d59842f1b34e6829d2c72cf9f
NovaWasTaKenn <98171103+NovaWasTaKenn@users.noreply.github.com>
2023-11-12 11:48:36 +0100

    save and delete script

---
+++ saveAndDelete.ps1
@@ -0,0 +1,36 @@
+# Define the resource group and Bicep file paths
+$resourceGroupList = @("YourResourceGroupName","YourResourceGroupName")
+$gitRepoUrl = "https://<username>:<password>@<git-repo-url>"
+
+foreach($resourceGroupName in $resourceGroupList){
+
+    $bicepFilePath = "D:\home\site\wwwroot\YourFunctionName\ResourceGroup.bicep"
+
+    # Authenticate using Managed Identity (Assuming Managed Identity is enabled for the Function App)
+    Connect-AzAccount -Identity
+
+    # Get the resource group
+    $resourceGroup = Get-AzResourceGroup -Name $resourceGroupName
+
+    # Convert the resource group to Bicep and save it to a file
+    $resourceGroup | ConvertTo-Bicep | Set-Content -Path $bicepFilePath
+
+    # Clone the Git repository
+    git clone $gitRepoUrl
+
+    # Move the Bicep file to the local repository
+    Move-Item $bicepFilePath -Destination "LocalGitRepository"
+
+    # Change to the Git repository directory
+    Set-Location -Path "LocalGitRepository"
+
+    # Add the Bicep file to the Git staging area
+    git add .
+
+}
+
+# Commit the changes
+git commit -m "Adding resource group Bicep file"
+
+# Push the changes to the remote repository
+git push
