d80109e458979dc0ad3fd35aeb6ac70ded8bab08
Quentin LE NESTOUR <quentin.le_nestour@edu.devinci.fr>
2023-11-12 12:52:44 +0100

    Adding resource group Bicep file

--- saveAndDelete.ps1
+++ saveAndDelete.ps1
@@ -1,10 +1,12 @@
 # Define the resource group and Bicep file paths
-$resourceGroupList = @("YourResourceGroupName","YourResourceGroupName")
-$gitRepoUrl = "https://<username>:<password>@<git-repo-url>"
+$resourceGroupList = @("RG1")
+$gitRepoUrl = "https://github.com/NovaWasTaKenn/HomeLabAzure.git"
 
 foreach($resourceGroupName in $resourceGroupList){
 
-    $bicepFilePath = "D:\home\site\wwwroot\YourFunctionName\ResourceGroup.bicep"
+    #$bicepFilePath = "D:\home\site\wwwroot\YourFunctionName\ResourceGroup.bicep"
+    $bicepFilePath = ".\backup\$($resourceGroupName).bicep"
+
 
     # Authenticate using Managed Identity (Assuming Managed Identity is enabled for the Function App)
     Connect-AzAccount -Identity
@@ -16,13 +18,15 @@ foreach($resourceGroupName in $resourceGroupList){
     $resourceGroup | ConvertTo-Bicep | Set-Content -Path $bicepFilePath
 
     # Clone the Git repository
-    git clone $gitRepoUrl
+    #git clone $gitRepoUrl
 
     # Move the Bicep file to the local repository
-    Move-Item $bicepFilePath -Destination "LocalGitRepository"
+    #Move-Item $bicepFilePath -Destination "LocalGitRepository"
+    #Move-Item $bicepFilePath -Destination "LocalGitRepository"
+
 
     # Change to the Git repository directory
-    Set-Location -Path "LocalGitRepository"
+    #Set-Location -Path "LocalGitRepository"
 
     # Add the Bicep file to the Git staging area
     git add .
