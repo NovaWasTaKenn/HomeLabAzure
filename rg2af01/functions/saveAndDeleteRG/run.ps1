e966466429f840e12faa3b7a20658ab03e2f9d85
Quentin LE NESTOUR <quentin.le_nestour@edu.devinci.fr>
2023-11-16 15:25:33 +0100

    git commit arm file

--- rg2af01/functions/saveAndDeleteRG/run.ps1
+++ rg2af01/functions/saveAndDeleteRG/run.ps1
@@ -8,8 +8,6 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-Import-Module Bicep
-
 Write-Information "RG list $($Request.Body.ResourceGroupListStr)"
 
 # Define the resource group and Bicep file paths 
@@ -21,9 +19,11 @@ $gitRepoUrl = $Request.Body.GitRepoUrl
 # Authenticate using Managed Identity (Assuming Managed Identity is enabled for the Function App)
 Connect-AzAccount -Identity
 
+
 foreach($resourceGroupName in $resourceGroupList){
 
     $armFilePath = "D:\home\site\wwwroot\saveAndDeleteRG\$($resourceGroupName).json"
+    $repoPath = "D:\home\site\wwwroot\saveAndDeleteRG\repo"
 
     #$bicepFilePath = ".\backup\$($resourceGroupName).json"
 
@@ -31,26 +31,26 @@ foreach($resourceGroupName in $resourceGroupList){
     Export-AzResourceGroup -ResourceGroupName $resourceGroupName -Path $armFilePath
 
     # Clone the Git repository
-    #git clone $gitRepoUrl
+    git clone -b main --single-branch $gitRepoUrl $repoPath
 
     # Move the Bicep file to the local repository
-    #Move-Item $bicepFilePath -Destination "LocalGitRepository"
-    #Move-Item $bicepFilePath -Destination "LocalGitRepository"
-
+    Move-Item $armFilePath -Destination $repoPath 
 
     # Change to the Git repository directory
-    #Set-Location -Path "LocalGitRepository"
+    Set-Location -Path $repoPath
 
-    # Add the Bicep file to the Git staging area
-    #git add .
+    git checkout main
+
+    # Add the arm file to the Git staging area
+    git add .
 
 }
 
 # Commit the changes
-#git commit -m "Adding resource group Bicep file"
+git commit -m "Adding resource group arm file"
 
 # Push the changes to the remote repository
-#git push
+git push
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
